1c1b7f93991ca7f305b77bd0aa636fd8d4768e75
Adam Keys <adam@therealadam.com>
2011-05-19 15:07:54 -0400

    Extract RSpec configure block.

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,6 +1,21 @@
 require 'sexyback'
 require 'cassandra/mock'
 
+RSpec.configure do
+
+  def schema
+    {
+      'Sexyback' => {
+        'Hash' => {
+          'comparator_type' => 'org.apache.cassandra.db.marshal.UTF8Type',
+          'column_type' => 'Standard'
+        }
+      }
+    }
+  end
+
+end
+
 describe Sexyback::Hash do
 
   before { Sexyback::Hash.connection = Cassandra::Mock.new('Sexyback', schema) }
